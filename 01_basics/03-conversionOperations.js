d8d06ae81b734353af80da380bbe3b646d469b4a
EMPTY-OSS <rajravi33200@gmail.com>
2025-03-30 07:01:32 +0000

    conversion of datatypes

---
+++ 01_basics/03-conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "hitesh"
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+//"33 = 33"
+//33abc = NaN
+// true = 1
+//false = 0
+
+
+let isloggedIn = "ravi"
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn);
+
+//1 = true
+//0 =false
+//string ('ravi)= true
+//empty string("") = false 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
