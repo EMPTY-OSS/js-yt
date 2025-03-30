aaeade03f42fb854f85620a1fe82707eda823cbe
EMPTY-OSS <rajravi33200@gmail.com>
2025-03-30 12:58:11 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+//console.log("2">1);
+//console.log("02">1);
+
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+
+// null is not zero so its not equal or greater than zero. or in third  statement null is defiend as zero so its not greater tha  zero.
+
+console.log(undefined==0);
+
+//undefined every time gives false
+
+// ===  it's checks the value strictely. means its check the vlause datatype.
+
+console.log("2" === 2);
+
