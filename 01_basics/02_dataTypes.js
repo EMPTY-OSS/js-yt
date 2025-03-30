24530bd15df8e13c7870f8f0f9d60db44b7f7616
EMPTY-OSS <rajravi33200@gmail.com>
2025-03-30 06:31:58 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";
+let name = "ravi"
+
+// nujmber = 2 to the power 53
+//bigint = when more number used
+// string => ""
+//boolean => true/false
+//null=> standalone value
+//undefined=> 
+//symbol = find uniqueness
+//object
+
+console.log(typeof "ravi");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
