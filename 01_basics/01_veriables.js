9ed0b5ce2aeee22da26664cb104a11821fdfc953
EMPTY-OSS <rajravi33200@gmail.com>
2025-03-30 06:33:01 +0000

    datatypes

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "ravi@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState;
+
+//accountId = 2
+//accountId = 2 not alllowed
+accountEmail = "hes@gmail.com"
+accountPassword = "12321"
+accountCity="sikar"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
