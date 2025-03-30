841291f455f704a7c066137eca8e91988f6b0dc8
EMPTY-OSS <rajravi33200@gmail.com>
2025-03-30 12:21:48 +0000

    conversionOperations

--- 01_basics/03-conversionOperations.js
+++ 01_basics/03-conversionOperations.js
@@ -22,7 +22,36 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//*********************OPERATIONS**************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " ravi"
+let str3 = str1+str2
+//console.log(str3);
+
+//console.log(1+"2");
+//console.log("1"+2);
+//console.log(1+2+"2");
+//console.log("1"+2+2);
+//console.log("1"+(2+2));
+
+//console.log(true);
+
+gameCounter = 100
+gameCounter--;
+console.log(gameCounter);
+
+
+
+
+
 
 
